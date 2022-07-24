0c49ffbad0a69a7e33ae762673f88d1d9afc0f9f
Iryna Kruts <i_kruts@ukr.net>
2022-07-25 00:08:06 +0300

    maked functions shorter

--- main.py
+++ main.py
@@ -1,37 +1,23 @@
+from typing import Union
+
+
 class Calculator:
-    def add(self, a, b):
+    def add(self, a: Union[float, int], b: Union[float, int]) -> Union[float, int]:
         pass
 
 
 class IntegerCalculator(Calculator):
-    def __init__(self):
-        self.sum = 0
-
-    def add(self, x, y):
-        self.sum += (x + y)
-
-    def __str__(self):
-        return f'{int(self.sum)}'
+    def add(self, x: Union[float, int], y: Union[float, int]) -> int:
+        return int(x + y)
 
 
 class FloatCalculator(Calculator):
-    def __init__(self):
-        self.sum = 0
-
-    def add(self, x, y):
-        self.sum += (x + y)
-
-    def __str__(self):
-        return f'{float(self.sum)}'
+    def add(self, x: Union[float, int], y: Union[float, int]) -> float:
+        return float(x + y)
 
 
-def make_add(fnc, num_1, num_2_2):
-    if isinstance(fnc, IntegerCalculator):
-        fnc.add(num_1, num_2_2)
-        return fnc
-    if isinstance(fnc, FloatCalculator):
-        fnc.add(num_1, num_2_2)
-        return fnc
+def make_add(fnc, num_1: Union[float, int], num_2_2: Union[float, int]) -> Union[float, int]:
+    return fnc.add(num_1, num_2_2)
 
 
 int_calc = IntegerCalculator()
